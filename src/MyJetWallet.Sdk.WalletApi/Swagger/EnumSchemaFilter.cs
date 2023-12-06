10df921436888c72844630521dc55ec48f3faa8a
Pavlo Pylypenko <pavel.p@smplt.net>
2023-12-06 17:15:57 +0200

    enum update

--- src/MyJetWallet.Sdk.WalletApi/Swagger/EnumSchemaFilter.cs
+++ src/MyJetWallet.Sdk.WalletApi/Swagger/EnumSchemaFilter.cs
@@ -11,20 +11,28 @@ public class EnumSchemaFilter : ISchemaFilter
 {
     public void Apply(OpenApiSchema model, SchemaFilterContext context)
     {
-        if (!context.Type.IsEnum) 
+        if (!context.Type.IsEnum)
             return;
-        
+
         model.Enum.Clear();
-        foreach (var enumName in Enum.GetNames(context.Type))
+        foreach (var enumValue in Enum.GetValues(context.Type))
         {
-            var memberInfo = context.Type.GetMember(enumName).FirstOrDefault(m => m.DeclaringType == context.Type);
+            var enumName = Enum.GetName(context.Type, enumValue);
+
+            var memberInfo = enumName == null
+                ? null
+                : context.Type.GetMember(enumName).FirstOrDefault(m => m.DeclaringType == context.Type);
+            
             var enumMemberAttribute = memberInfo == null
                 ? null
                 : memberInfo.GetCustomAttributes(typeof(EnumMemberAttribute), false).OfType<EnumMemberAttribute>()
                     .FirstOrDefault();
+
             var label = enumMemberAttribute == null || string.IsNullOrWhiteSpace(enumMemberAttribute.Value)
                 ? enumName
                 : enumMemberAttribute.Value;
+
+            label = $"{label} ({(int) enumValue})";
             model.Enum.Add(new OpenApiString(label));
         }
     }
