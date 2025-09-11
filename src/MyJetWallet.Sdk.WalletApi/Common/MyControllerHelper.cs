523f983253d22651661e92b373c7e0779fe1898d
Oleksandr Holets <oleksandr.h@simple.app>
2025-09-11 13:08:10 +0300

    add MyControllerHelper

---
+++ src/MyJetWallet.Sdk.WalletApi/Common/MyControllerHelper.cs
@@ -0,0 +1,19 @@
+using MyJetWallet.Sdk.WalletApi.Contracts;
+
+namespace MyJetWallet.Sdk.WalletApi.Common;
+
+public static class MyControllerHelper
+{
+    public static void ShouldBe(bool result, string paramText, object paramValue)
+    {
+        if (!result)
+        {
+            throw new WalletApiErrorException($"Wrong parameter value. {paramText} = {paramValue}", ApiResponseCodes.ValidationError);
+        }
+    }
+
+    public static bool IsNotEmpty(this string s)
+    {
+        return !string.IsNullOrEmpty(s);
+    }
+}
